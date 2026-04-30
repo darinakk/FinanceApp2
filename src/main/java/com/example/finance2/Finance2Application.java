6cbe4ece8a25dabf81d5f3f012e2aa3e27cc52ce
Darin Abdullah <darinabd@ug.kth.se>
2026-04-30 16:35:46 +0200

    spring-boot projected created

---
+++ src/main/java/com/example/finance2/Finance2Application.java
@@ -0,0 +1,13 @@
+package com.example.finance2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Finance2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Finance2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finance2/Finance2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finance2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Finance2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
