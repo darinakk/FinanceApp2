002eb4bd7621a0ac6bdde8a1bf4546d81167d17d
Darin Abdullah <darinabd@ug.kth.se>
2026-05-04 15:52:51 +0200

    ändringarna

--- src/main/java/com/example/finance2/Transaction.java
+++ src/main/java/com/example/finance2/Transaction.java
@@ -2,25 +2,25 @@ package com.example.finance2;
 
 import jakarta.persistence.*;
 
-@Entity
-public class Transaction {
+@Entity     //ska bli tabell i databas säger till spring boot, utan går det ej
+public class Transaction {  //i sql ska  tabell med namnet transactions se ut såhär
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Id     // hur databasen ska hitta rätt, som ett personnummer, unikt
+    @GeneratedValue(strategy = GenerationType.IDENTITY)     // varje rad ska få id automatiskt
+    private Long id;        // variabeln long som sparar nummrne
 
-    private String description;
-    private double amount;
+    private String description;     //data vi vill spara
+    private double amount;      //getters and setters
 
-    public Long getId() {
+    public Long getId() {   // hämta data
         return id;
     }
 
     public String getDescription() {
-        return description;
+        return description;     // metioden ger tillbaka description
     }
 
-    public void setDescription(String description) {
+    public void setDescription(String description) {        //ändra description
         this.description = description;
     }
 
